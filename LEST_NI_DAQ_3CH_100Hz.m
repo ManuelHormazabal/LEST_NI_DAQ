ba44f7d4c954e6ee2c3d7424095094253e4eb91a
Manuel F. Hormazabal <101811859+ManuelHormazabal@users.noreply.github.com>
2024-09-08 10:25:33 +0100

    Update LEST_NI_DAQ_3CH_100Hz.m
    Signed-off-by: Manuel F. Hormazabal <101811859+ManuelHormazabal@users.noreply.github.com>

--- LEST_NI_DAQ_3CH_100Hz.m
+++ LEST_NI_DAQ_3CH_100Hz.m
@@ -3,26 +3,31 @@
 % Author    :  Manuel F. Hormazabal
 % Contact   :  m.hormazabal@gmail.com
 %------------------------------------------------------------------------------------------
+% Code for a three channel acquisition with PCB393B12 accelerometers and generate an output
+% matrix with acceleration data sampled at 100 Hz using the NI 9234 ADC.
+% Transducers : #16906 - #16910 - #32170 (check lab. data for updated sensitivities)
+%------------------------------------------------------------------------------------------
+%
 clear all; close all; clc;
 %
 %% Test Setup -----------------------------------------------------------------------------
 % 
-    Config.Test_Name = 'PhD_LAB_Damaged';
+    Config.Test_Name = 'test_name';
     Config.Test_Date = 05052022; 
-    % 
+    
     % --- Input channels
     NI_DAQ = daq('ni');
     CH_01 = addinput(NI_DAQ,'Dev1','ai1','Accelerometer');
     CH_02 = addinput(NI_DAQ,'Dev1','ai2','Accelerometer');
     CH_03 = addinput(NI_DAQ,'Dev1','ai3','Accelerometer');
     NI_DAQ.Rate = 51200;
-    %
-    % --- Transducer sensitivity (check in "the excel file")
-    CH_01.Sensitivity = 9.59969; % PCB393B12 #16906 
-    CH_02.Sensitivity = 9.52562; % PCB393B12 #16910
-    CH_03.Sensitivity = 9.69932; % PCB393B12 #32170
-    % 
-    % --- Records
+    
+    % --- Transducers sensitivity (check updated info in "the excel file")
+    CH_01.Sensitivity = 9.59969;     % PCB393B12 #16906 
+    CH_02.Sensitivity = 9.52562;     % PCB393B12 #16910
+    CH_03.Sensitivity = 9.69932;     % PCB393B12 #32170
+    
+    % --- Acquisitions
     Config.Number_of_records = 42;
     Config.Recording_Time    = 600;  
     Config.Setting_Pause     = 60;
@@ -48,7 +53,7 @@ clear all; close all; clc;
             Data_800_TEMP   = decimate(Data_1600_TEMP ,2,Config.Filter_Order); 
             Data_400_TEMP   = decimate(Data_800_TEMP  ,2,Config.Filter_Order); 
             Data_200(k,:,j) = decimate(Data_400_TEMP  ,2,Config.Filter_Order); 
-            Data_100(k,:,j) = decimate(Data_200(k,:,j),2,Config.Filter_Order); % DEFAULT OUTPUT [Fs = 100 Hz]
+            Data_100(k,:,j) = decimate(Data_200(k,:,j),2,Config.Filter_Order);
         end
         if k < 10 
             writematrix(squeeze(Data_100(k,:,:)),[Config.Test_Name,'_',...
